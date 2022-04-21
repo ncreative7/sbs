b1e97cb3a8c44553fac3eebf47fe50acfbf208b8
ncreative7 <96519798+ncreative7@users.noreply.github.com>
2022-04-21 20:52:11 +0900

    Add files via upload

---
+++ slick2.js
@@ -0,0 +1,69 @@
+$(document).ready(function(){
+  
+    $(".slider").slick({
+      dots: true ,
+      // prevArrow: '<div class="left-btn"></div>' ,
+      // nextArrow: '<div class="right-btn"></div>' ,
+      // prevArrow: $(".left-btn") ,
+      // nextArrow: $(".right-btn") ,
+      arrows: false ,
+      customPaging: function(slide , i){
+        let slide_num = $(slide.$slides[i]).attr("data-dots-txt")
+        
+        console.log(slide_num)
+        
+        return slide_num
+      } ,
+    })
+    
+    // 상황 left-btn 을 클릭하면.. 기능이 작동한다.
+    // 기능 슬라이드가 이전으로 간다.
+    
+    $(".left-btn").click(function(){
+      
+      $('.slider').slick('slickPrev')
+      
+    })
+    $(".right-btn").click(function(){
+      
+      $('.slider').slick('slickNext')
+      
+    })
+    
+    $(".play").click(function(){
+      
+      $(".play").removeClass("active")
+      $(".pause").addClass("active")
+      $('.slider').slick('slickPlay')
+      
+    })
+      $(".pause").click(function(){
+      
+      $(".play").addClass("active")
+      $(".pause").removeClass("active")
+      $('.slider').slick('slickPause')
+      
+    })
+    
+    $(".dots > .dot").click(function(){
+      
+      let num = $(this).index()
+      console.log(num)
+      
+      $('.slider').slick('slickGoTo' , num);
+    })
+    
+    
+    $('.slider').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+      console.log("비포체인지");
+      $(".dot5").text( nextSlide+1 + " / 4")
+      $(".dots > .dot").eq(nextSlide).addClass("active")
+      $(".dots > .dot").eq(nextSlide).siblings().removeClass("active")
+    });
+  
+    $('.slider').on('afterChange', function(event, slick, currentSlide){
+      console.log("에프터체인지");
+    });
+    
+    
+  })
